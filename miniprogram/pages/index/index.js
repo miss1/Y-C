9e131d85275e0dc6518af7fdf55e02ec2981df84
yang <yanglovecici@163.com>
2019-11-28 17:36:37 +0800

    login

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,6 +3,44 @@ const app = getApp()
 
 Page({
   data: {
-    text: 'Y&C'
-  }
+    text: 'Y&C',
+    avatarUrl: '',
+    userInfo: ''
+  },
+  onLoad: function () {
+    wx.cloud.callFunction({
+      name: 'login',
+      complete: res => {
+        app.globalData.openid = res.result.openid;
+        console.log(app.globalData.openid);
+      }
+    })
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  onGetUserInfo: function (e) {
+    console.log(e.detail.userInfo);
+    if (e.detail.userInfo) {
+      this.setData({
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo
+      })
+    }
+  },
 })
